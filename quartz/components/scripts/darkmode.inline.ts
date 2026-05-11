5de92db12c3756833d4dc3b1e72c0595d049778c
JinGoo <zzgg3040@naver.com>
2026-05-11 19:37:38 +0900

    feat: add frontmatter dates, force dark mode, sidebar resize handle fix

--- quartz/components/scripts/darkmode.inline.ts
+++ quartz/components/scripts/darkmode.inline.ts
@@ -1,6 +1,5 @@
-const userPref = window.matchMedia("(prefers-color-scheme: light)").matches ? "light" : "dark"
-const currentTheme = localStorage.getItem("theme") ?? userPref
-document.documentElement.setAttribute("saved-theme", currentTheme)
+localStorage.setItem("theme", "dark")
+document.documentElement.setAttribute("saved-theme", "dark")
 
 const emitThemeChangeEvent = (theme: "light" | "dark") => {
   const event: CustomEventMap["themechange"] = new CustomEvent("themechange", {
